c6edc6d78f0169df7ca52b752b6c6cd51c400c72
luansevla <luanalvess01@gmail.com>
2026-05-18 20:14:13 -0300

    Auth added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthResolver } from './auth/auth.resolver';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, LoginModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthResolver],
 })
 export class AppModule {}
